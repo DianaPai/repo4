bbbec8355c60a8f10fde73f5f53babb4f0c9d4d9
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-04-29 15:54:36 -0700

    Pushed from Domino: https://sup601gh75734.support-team-sandbox.domino.tech/workspace/integration-test/diana-git?executionId=68115765f5b02e071e4aa171

--- call_python.sh
+++ call_python.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python add_comment_and_commit.py helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
+# python add_comment_and_commit.py helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
+python /mnt/imported/code/repo4/add_comment_and_commit.py /mnt/imported/code/repo4/helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
